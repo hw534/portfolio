02fc4cc19083d779d590b703546da6999775a384
hw534 <waynehw534@gmail.com>
2023-01-27 01:37:43 -0400

    Nueva version del portafolio

---
+++ app.py
@@ -0,0 +1,12 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+app.run()
